004177b497d3098a077dab7bb6113c299eebfde9
yinxiaowei <373736762@qq.com>
2018-03-20 18:09:36 +0800

    头部和底部导航栏的提交。刷新还存在问题。。。

--- app/src/main/java/com/uurobot/yxwlib/car/CarMainActivity.java
+++ app/src/main/java/com/uurobot/yxwlib/car/CarMainActivity.java
@@ -2,21 +2,19 @@ package com.uurobot.yxwlib.car;
 
 import android.os.Bundle;
 import android.support.annotation.Nullable;
-import android.support.design.widget.TabLayout;
-import android.support.v4.app.Fragment;
-import android.support.v4.view.ViewPager;
+import android.support.v4.app.FragmentManager;
+import android.support.v4.app.FragmentTransaction;
+import android.util.Log;
+import android.widget.FrameLayout;
 import android.widget.ImageView;
 import android.widget.SearchView;
-import android.widget.TableLayout;
+import android.widget.TextView;
 
 import com.uurobot.yxwlib.R;
 
-import java.util.ArrayList;
-import java.util.List;
-
 import butterknife.BindView;
 import butterknife.ButterKnife;
-
+import butterknife.OnClick;
 
 
 /**
@@ -24,7 +22,10 @@ import butterknife.ButterKnife;
  */
 
 public class CarMainActivity extends BaseActivity {
-        public static final String[] tabTitle = new String[]{"综艺1","体育2","新闻3","热点4","头条5","军事6","历史7","科技8","人文9"};
+
+        private static final String TAG = "CarMainActivity";
+        public static final String[] tabTitle = new String[]{"综艺1", "体育2", "新闻3", "热点4", "头条5", "军事6", "历史7", "科技8", "人文9"};
+
         @BindView(R.id.search_car)
         SearchView searchCar;
 
@@ -34,28 +35,64 @@ public class CarMainActivity extends BaseActivity {
         @BindView(R.id.right)
         ImageView right;
 
-        @BindView(R.id.tablayout_car)
-        TabLayout tablayoutCar;
+        @BindView(R.id.frameLayout)
+        FrameLayout frameLayout;
 
-        @BindView(R.id.viewPager_car)
-        ViewPager viewPagerCar;
+        FirstFragment firstFragment;
+        SecondeFragment secondeFragment;
 
-        TabAdapter tabAdapter ;
+        @BindView(R.id.bottom_car_1)
+        TextView bottomCar1;
 
+        @BindView(R.id.bottom_car_2)
+        TextView bottomCar2;
+
+        @BindView(R.id.bottom_car_3)
+        TextView bottomCar3;
+
+        @BindView(R.id.bottom_car_4)
+        TextView bottomCar4;
+
+        @BindView(R.id.bottom_car_5)
+        TextView bottomCar5;
+        FragmentManager supportFragmentManager;
+        int index =1 ;
         @Override
         protected void onCreate(@Nullable Bundle savedInstanceState) {
                 super.onCreate(savedInstanceState);
                 setContentView(R.layout.activity_car);
                 ButterKnife.bind(this);
-                List<Fragment> fragments = new ArrayList<>();
-                for (int i=0,len = tabTitle.length; i<len ; i++){
-                        fragments.add(TabLayoutFragment.newInstance(i));
+
+
+                firstFragment = new FirstFragment();
+                secondeFragment = new SecondeFragment();
+                 supportFragmentManager = getSupportFragmentManager();
+                FragmentTransaction fragmentTransaction = supportFragmentManager.beginTransaction();
+                fragmentTransaction.replace(R.id.frameLayout, firstFragment).commit();
+                bottomCar1.setPressed(true);
+        }
+
+        @OnClick(R.id.bottom_car_1)
+        public void first(){
+               if (index != 1 ){
+                       FragmentTransaction fragmentTransaction = supportFragmentManager.beginTransaction();
+                       fragmentTransaction.replace(R.id.frameLayout, firstFragment).commit();
+                       bottomCar1.setPressed(true);
+                       bottomCar2.setPressed(false);
+                       index =  1;
+                       Log.e(TAG, "first:  press");
+               }
+        }
+
+        @OnClick(R.id.bottom_car_2)
+        public void second(){
+                if (index != 2 ){
+                        FragmentTransaction fragmentTransaction = supportFragmentManager.beginTransaction();
+                        fragmentTransaction.replace(R.id.frameLayout, secondeFragment).commit();
+                        bottomCar1.setPressed(false);
+                        bottomCar2.setPressed(true);
+                        index =  2 ;
+                        Log.e(TAG, "second: press" );
                 }
-
-                tabAdapter = new TabAdapter(getSupportFragmentManager(),fragments);
-
-                viewPagerCar.setAdapter(tabAdapter);
-                tablayoutCar.setupWithViewPager(viewPagerCar);
-                tablayoutCar.setTabMode(TabLayout.MODE_SCROLLABLE);
         }
 }
--- app/src/main/java/com/uurobot/yxwlib/car/SecondeFragment.java
+++ app/src/main/java/com/uurobot/yxwlib/car/SecondeFragment.java
@@ -2,14 +2,17 @@ package com.uurobot.yxwlib.car;
 
 import android.os.Bundle;
 import android.support.annotation.Nullable;
+import android.support.design.widget.TabLayout;
 import android.support.v4.app.Fragment;
-import android.util.Log;
+import android.support.v4.view.ViewPager;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
-import android.widget.TextView;
 
 import com.uurobot.yxwlib.R;
+
+import java.util.ArrayList;
+import java.util.List;
 
 import butterknife.BindView;
 import butterknife.ButterKnife;
@@ -19,68 +22,39 @@ import butterknife.Unbinder;
  * Created by Administrator on 2018/3/16.
  */
 
-public class TabLayoutFragment extends Fragment {
+public class SecondeFragment extends Fragment {
 
         private static final String TAG = "TabLayoutFragment";
+
         private static String key = "TAB";
 
-        @BindView(R.id.frament_tv_car)
-        TextView framentTvCar;
 
         Unbinder bind;
 
-        private int type;
+
 
         @Nullable
         @Override
         public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
 
                 // 这里只能传递false，否则报错 ？？？？？？？？？？？？？？？？？
-                View inflate = inflater.inflate(R.layout.fragment_car, container, false);
+                View inflate = inflater.inflate(R.layout.car_second_fragment, container, false);
                 bind = ButterKnife.bind(this, inflate);
                 initView(inflate);
-                Log.e(TAG, "onCreateView: "+type );
                 return inflate;
         }
 
         private void initView(View inflate) {
-                Log.e(TAG, "initView: "+type );
 //                TextView textView = inflate.findViewById(R.id.frament_tv_car);
 //                textView.setText(CarMainActivity.tabTitle[type]);
-                 framentTvCar.setText("这是="+CarMainActivity.tabTitle[type]);
         }
 
-
-        public static TabLayoutFragment newInstance(int type) {
-                TabLayoutFragment tabLayoutFragment = new TabLayoutFragment();
-                Bundle bundle = new Bundle();
-                bundle.putSerializable(key, type);
-                tabLayoutFragment.setArguments(bundle);
-                return tabLayoutFragment;
-        }
-
-        @Override
-        public void onResume() {
-                super.onResume();
-                Log.e(TAG, "onResume: "+type );
-        }
-
-        @Override
-        public void onPause() {
-                super.onPause();
-                Log.e(TAG, "onPause: "+type );
-        }
 
         @Override
         public void onCreate(@Nullable Bundle savedInstanceState) {
                 super.onCreate(savedInstanceState);
                 Bundle arguments = getArguments();
-                if (arguments != null) {
-                        type = (int) arguments.getSerializable(key);
-                        Log.e(TAG, "onCreate: "+type  );
-                }else {
-                        Log.e(TAG, "onCreate: arguments is null =="+type  );
-                }
+
 
         }
 
@@ -88,6 +62,5 @@ public class TabLayoutFragment extends Fragment {
         public void onDestroyView() {
                 super.onDestroyView();
                 bind.unbind();
-                Log.e(TAG, "onDestroyView: "+type );
         }
 }
