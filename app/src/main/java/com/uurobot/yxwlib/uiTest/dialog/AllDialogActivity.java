816b12a1e2d751e311d191c2c6d9c676714698a3
yinxiaowei <373736762@qq.com>
2018-03-29 17:48:48 +0800

    数据库测试。。。。。。。              。。。。。。。。。。。。。

--- app/src/main/java/com/uurobot/yxwlib/uiTest/dialog/AllDialogActivity.java
+++ app/src/main/java/com/uurobot/yxwlib/uiTest/dialog/AllDialogActivity.java
@@ -2,26 +2,20 @@ package com.uurobot.yxwlib.uiTest.dialog;
 
 import android.app.Activity;
 import android.content.DialogInterface;
-import android.content.res.AssetFileDescriptor;
-import android.content.res.AssetManager;
-import android.media.MediaPlayer;
-import android.net.Uri;
 import android.os.Bundle;
-import android.os.Environment;
 import android.support.annotation.Nullable;
-import android.util.Log;
+import android.support.constraint.ConstraintLayout;
+import android.util.DisplayMetrics;
+import android.view.LayoutInflater;
+import android.view.View;
 import android.widget.Button;
 
 import com.afollestad.materialdialogs.MaterialDialog;
-import com.afollestad.materialdialogs.StackingBehavior;
+import com.loonggg.bottomsheetpopupdialoglib.ShareBottomPopupDialog;
 import com.uurobot.yxwlib.R;
-import com.uurobot.yxwlib.alarm.Logger;
 import com.uurobot.yxwlib.util.ToastUtil;
 
-import java.io.File;
-import java.io.FileDescriptor;
-import java.io.IOException;
-import java.io.InputStream;
+import org.utils.common.util.DensityUtil;
 
 import butterknife.BindView;
 import butterknife.ButterKnife;
@@ -33,74 +27,38 @@ import butterknife.OnClick;
 
 public class AllDialogActivity extends Activity {
 
-        private static final java.lang.String TAG = "AllDialogActivity";
+        private static final String TAG = "AllDialogActivity";
 
         @BindView(R.id.button6)
         Button button6;
+
+        @BindView(R.id.allLayout)
+        ConstraintLayout allLayout;
 
         @Override
         protected void onCreate(@Nullable Bundle savedInstanceState) {
                 super.onCreate(savedInstanceState);
                 setContentView(R.layout.activity_all_dialog);
                 ButterKnife.bind(this);
-                play();
         }
 
         @OnClick(R.id.button6)
-        public void setButton6(){
+        public void setButton6() {
 //                choiceDialog();
+                testBottomDialog();
         }
 
-        private void play(){
-//                final MediaPlayer mMediaPlayer = new MediaPlayer();
-//                if (mMediaPlayer != null) {
-//                        try {
-//                                String path = Environment.getExternalStorageDirectory()+"/ai/ai05res/a1/res/audio/dance/chisx.mp3";
-////                                String path = Environment.getExternalStorageDirectory()+"/ai/chisx.mp3";
-//
-////                                AssetManager assets = getAssets();
-////                                AssetFileDescriptor open = assets.openFd("vivi3.mp4");
-////
-////                                mMediaPlayer.setDataSource(open.getFileDescriptor(),0,open.getLength()/2);
-//                                mMediaPlayer.setDataSource(path);
-//
-//                                mMediaPlayer.setOnPreparedListener(new MediaPlayer.OnPreparedListener() {
-//                                        @Override
-//                                        public void onPrepared(MediaPlayer mp) {
-//                                                Log.e(TAG, "onPrepared: " );
-//                                                mMediaPlayer.start();
-//                                        }
-//                                });
-//                                mMediaPlayer.setOnErrorListener(new MediaPlayer.OnErrorListener() {
-//                                        @Override
-//                                        public boolean onError(MediaPlayer mp, int what, int extra) {
-//                                                Log.e(TAG, "onError: ");
-//                                                return false;
-//                                        }
-//                                });
-//                                mMediaPlayer.setOnCompletionListener(new MediaPlayer.OnCompletionListener() {
-//                                        @Override
-//                                        public void onCompletion(MediaPlayer mp) {
-//                                                Log.e(TAG, "onCompletion: " );
-//                                        }
-//                                });
-//
-//                        } catch (IOException e) {
-//                                e.printStackTrace();
-//                                Logger.e(TAG, "play Exception---"+e);
-//                        }
-//                }
 
-                File file = new File(Environment.getExternalStorageDirectory()+"/ai/ai05res/a1/res/audio/dance/mj.wav");
-                final MediaPlayer mediaPlayer = MediaPlayer.create(this, Uri.fromFile(file));
-                mediaPlayer.setOnPreparedListener(new MediaPlayer.OnPreparedListener() {
-                        @Override
-                        public void onPrepared(MediaPlayer mp) {
-                                mediaPlayer.start();
-                        }
-                });
-
+        private void testBottomDialog() {
+                LayoutInflater inflater = LayoutInflater.from(this);
+                View inflate = inflater.inflate(R.layout.dialog_bottom_test, null);
+                ShareBottomPopupDialog shareBottomPopupDialog = new ShareBottomPopupDialog(this,inflate);
+                DisplayMetrics displayMetrics = getResources().getDisplayMetrics();
+                int heightPixels = displayMetrics.heightPixels;
+                shareBottomPopupDialog.getmPopupWindow().setHeight(heightPixels/2);
+                shareBottomPopupDialog.showPopup(allLayout);
         }
+
 
         private void baseDialog() {
                 new MaterialDialog.Builder(this)
