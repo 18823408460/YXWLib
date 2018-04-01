43041484cd41f8d16d39bd9fae3549c491d37259
373736762 <373736762@qq.com>
2018-04-01 11:34:24 +0800

    aidl test  相关的提交。。。。。。。

---
+++ app/src/main/aidl/com/uurobot/yxwlib/ICallBack.java
@@ -0,0 +1,9 @@
+package com.uurobot.yxwlib;
+
+/**
+ * Created by WEI on 2018/4/1.
+ */
+
+public interface ICallBack {
+    void onReceiver(String data);
+}
---
+++ app/src/main/aidl/com/uurobot/yxwlib/IRegister.java
@@ -0,0 +1,11 @@
+package com.uurobot.yxwlib;
+
+/**
+ * Created by WEI on 2018/4/1.
+ */
+
+import com.uurobot.yxwlib.ICallBack;
+
+public interface IRegister {
+    void register(ICallBack iCallBack);
+}
