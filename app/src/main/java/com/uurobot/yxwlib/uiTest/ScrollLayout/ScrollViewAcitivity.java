fd81ede19ea548d46ad1c8bf3a0cff43dd7b1619
yinxiaowei <373736762@qq.com>
2018-04-04 16:06:09 +0800

    shape相关的笔记的的提交。。。。。。。。。。。              。。。。。。。。。。。。。

---
+++ app/src/main/java/com/uurobot/yxwlib/uiTest/ScrollLayout/ScrollViewAcitivity.java
@@ -0,0 +1,20 @@
+package com.uurobot.yxwlib.uiTest.ScrollLayout;
+
+import android.app.Activity;
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+
+import com.uurobot.yxwlib.R;
+
+/**
+ * Created by Administrator on 2018/4/4.
+ */
+
+public class ScrollViewAcitivity extends Activity {
+
+        @Override
+        protected void onCreate(@Nullable Bundle savedInstanceState) {
+                super.onCreate(savedInstanceState);
+                setContentView(R.layout.activity_scrolllayout);
+        }
+}
